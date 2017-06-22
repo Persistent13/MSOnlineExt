967d7013ed77907d3ccc6c8e87a0b30c26ab0fa6
Dakota Clark <dakota@allclarks.com>
2017-06-22 14:03:52 -0700

    Added tenant context cmdlets.

--- src/MSOnline-Extensions/Public/Remove-MsolTenantContext.ps1
+++ src/MSOnline-Extensions/Public/Remove-MsolTenantContext.ps1
@@ -1,2 +1,39 @@
-# TODO: Write cmdlet that will set
-# $PSDefaultParameterValues.Remove('*-Msol*:TenantId')
+function Remove-MsolTenantContext
+{
+    [CmdletBinding(SupportsShouldProcess=$true,
+                   PositionalBinding=$false,
+                   ConfirmImpact='Low')]
+    Param()
+
+    Begin
+    {
+        if($PSDefaultParameterValues['*-Msol*:TenantId'] -eq $null)
+        {
+            Write-Verbose -Message 'Default TenantId has not been set. Please run Set-MsolTenantContext.'
+            $target = '00000000-0000-0000-0000-000000000000'
+        }
+        else
+        {
+            $target = $PSDefaultParameterValues['*-Msol*:TenantId']
+        }
+    }
+
+    Process
+    {
+        if($PSCmdlet.ShouldProcess($target,'Remove default tenant ID.'))
+        {
+            try
+            {
+                $PSDefaultParameterValues.Remove('*-Msol*:TenantId')
+            }
+            catch
+            {
+                $PSCmdlet.ThrowTerminatingError($PSItem)
+            }
+        }
+    }
+
+    End
+    {
+    }
+}
--- src/MSOnline-Extensions/Public/Set-MsolTenantContext.ps1
+++ src/MSOnline-Extensions/Public/Set-MsolTenantContext.ps1
@@ -1,4 +1,43 @@
-# TODO: Write cmdlet that will set
-# $PSDefaultParameterValues.Add('*-Msol*:TenantId','CLIENT GUID')
-# It will then reset the argument completers
-# e.g. update the Upn resolution to the new tenant
+function Set-MsolTenantContext
+{
+    [CmdletBinding(SupportsShouldProcess=$false,
+                   PositionalBinding=$false,
+                   ConfirmImpact='Low')]
+    Param
+    (
+        [Parameter(Mandatory,
+                   ValueFromPipeline=$false,
+                   ValueFromPipelineByPropertyName=$false,
+                   ValueFromRemainingArguments=$false,
+                   Position=0)]
+        [Guid]$TenantId
+    )
+
+    Begin
+    {
+    }
+
+    Process
+    {
+        try
+        {
+            $PSDefaultParameterValues.Add('*-Msol*:TenantId',$TenantId)
+
+            $completers = Get-ChildItem -Path "$PSScriptRoot\..\Completers\" -Filter '*.ps1' -ErrorAction SilentlyContinue
+            foreach($item in $completers)
+            {
+                $message = 'Re-importing Completer: {0}' -f $item.FullName
+                Write-Verbose -Message $message
+                & $item.FullName
+            }
+        }
+        catch
+        {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+
+    End
+    {
+    }
+}
