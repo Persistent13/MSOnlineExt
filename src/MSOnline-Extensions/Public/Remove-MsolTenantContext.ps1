ddd3ff99779fe6757fc48aaf13ceb29eca39a195
Dakota Clark <dakota@allclarks.com>
2017-06-22 07:47:02 -0700

    Starting context cmdlets.

---
+++ src/MSOnline-Extensions/Public/Remove-MsolTenantContext.ps1
@@ -0,0 +1,2 @@
+# TODO: Write cmdlet that will set
+# $PSDefaultParameterValues.Remove('*-Msol*:TenantId')
---
+++ src/MSOnline-Extensions/Public/Set-MsolTenantContext.ps1
@@ -0,0 +1,4 @@
+# TODO: Write cmdlet that will set
+# $PSDefaultParameterValues.Add('*-Msol*:TenantId','CLIENT GUID')
+# It will then reset the argument completers
+# e.g. update the Upn resolution to the new tenant
