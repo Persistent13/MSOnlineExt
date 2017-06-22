3570be7e2a045e769ee265be56bd5d766a532ba4
Dakota Clark <dakota@allclarks.com>
2017-06-22 08:24:01 -0700

    Now with 100% more pre-remove cleanup.

--- src/MSOnline-Extensions/MSOnline-Extensions.psm1
+++ src/MSOnline-Extensions/MSOnline-Extensions.psm1
@@ -6,3 +6,7 @@ foreach($item in $completers)
     Write-Verbose -Message $message
     & $item.FullName
 }
+
+# This code will run when the module is removed and
+# will be sure that the TenantId default is removed
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = { $PSDefaultParameterValues.Remove('*-Msol*:TenantId') }
