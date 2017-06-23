eff92cbf9bf7da97853f9d149d7fcd78a4cc3acd
Dakota Clark <dakota@allclarks.com>
2017-06-23 15:35:38 -0700

    Skipping FileList.

--- MSOnlineExt.build.ps1
+++ MSOnlineExt.build.ps1
@@ -32,14 +32,11 @@ task UnloadModule {
 task UpdateManifest {
     $functions_path = Join-Path -Path ( Join-Path -Path ( Join-Path -Path $PSScriptRoot -ChildPath 'src' ) -ChildPath 'MSOnlineExt' ) -ChildPath 'Public'
     $manifest_path = Join-Path -Path ( Join-Path -Path ( Join-Path -Path $PSScriptRoot -ChildPath 'src' ) -ChildPath 'MSOnlineExt' ) -ChildPath 'MSOnlineExt.psd1'
-    $files_path = Join-Path -Path ( Join-Path -Path $PSScriptRoot -ChildPath 'src' ) -ChildPath 'MSOnlineExt'
 
     $functions = Get-ChildItem -Path $functions_path -Filter '*.ps1'
-    $files = Get-ChildItem -Path $files_path -File -Recurse
     $manifest_params = @{
         Path = $manifest_path
         Copyright = '(c) {0} Dakota Clark. All rights reserved.' -f (Get-Date).Year
-        FileList = $files.Name
         FunctionsToExport = $functions.BaseName
         ModuleVersion = $env:APPVEYOR_BUILD_VERSION
     }
