f73d6953e9811b7e3f9d8c16471fa5ef8aeff3e0
Dakota Clark <dakota@allclarks.com>
2017-06-23 12:41:03 -0700

    All tests created and updated.

--- src/Test/MSOnlineExt.Tests.ps1
+++ src/Test/MSOnlineExt.Tests.ps1
@@ -1,8 +1,8 @@
-$test_guid = New-Guid
-$test_guid_empty = [guid]::Empty
-$test_guid_static = '230a7523-6117-4f5f-9209-7fb5807416f1'
+$global:test_guid = New-Guid
+$global:test_guid_empty = [guid]::Empty
+$global:test_guid_static = '230a7523-6117-4f5f-9209-7fb5807416f1'
 
-$module_path = Join-Path -Path ( Join-Path -Path $PSScriptRoot -ChildPath 'MSOnlineExt' ) -ChildPath 'MSOnlineExt.psd1'
+$module_path = Join-Path -Path ( Join-Path -Path ( Split-Path -Path $PSScriptRoot -Parent ) -ChildPath 'MSOnlineExt' ) -ChildPath 'MSOnlineExt.psd1'
 
 Import-Module 'MSOnline'
 Import-Module $module_path
@@ -11,44 +11,51 @@ InModuleScope MSOnlineExt {
     Describe 'Get-MsolTenantContext' {
         Context 'Set-MsolTenantContext has been run' {
 
-            Mock Set-MsolTenantContext { $global:PSDefaultParameterValues['*-Msol*:TenantId'] = $test_guid }
-            Set-MsolTenantContext -TenantId $test_guid_static
-
-            Assert-MockCalled Set-MsolTenantContext
-
+            Mock Set-MsolTenantContext { $global:PSDefaultParameterValues['*-Msol*:TenantId'] = $global:test_guid } -Verifiable
+            Set-MsolTenantContext -TenantId $global:test_guid_static
+            It 'Set-MsolTenantContext has been run' {
+                Assert-VerifiableMocks
+            }
             It 'Returns the correct Guid' {
-                Get-MsolTenantContext | Should Be $test_guid
+                Get-MsolTenantContext | Should Be $global:test_guid
             }
         }
         Context 'Set-MsolTenantContext has NOT been run' {
+
+            $global:PSDefaultParameterValues.Remove('*-Msol*:TenantId')
+
             It 'Returns the correct Guid' {
-                Get-MsolTenantContext | Should Be $test_guid_empty
+                Get-MsolTenantContext | Should Be $global:test_guid_empty
             }
         }
     }
     Describe 'Remove-MsolTenantContext' {
         Context 'Set-MsolTenantContext has been run' {
 
-            Mock Set-MsolTenantContext { $global:PSDefaultParameterValues['*-Msol*:TenantId'] = $test_guid }
-            Set-MsolTenantContext -TenantId $test_guid_static
+            Mock Set-MsolTenantContext { $global:PSDefaultParameterValues['*-Msol*:TenantId'] = $global:test_guid } -Verifiable
+            Set-MsolTenantContext -TenantId $global:test_guid_static
 
-            Assert-MockCalled Set-MsolTenantContext
-
+            It 'Set-MsolTenantContext has been run' {
+                Assert-VerifiableMocks
+            }
             It 'Removes the default Guid' {
                 Remove-MsolTenantContext
                 $global:PSDefaultParameterValues['*-Msol*:TenantId'] | Should Be $null
             }
         }
         Context 'Set-MsolTenantContext has NOT been run' {
-            It 'Throws an error' {
-                { Remove-MsolTenantContext } | Should Throw
+
+            $global:PSDefaultParameterValues.Remove('*-Msol*:TenantId')
+
+            It 'No error is thrown' {
+                { Remove-MsolTenantContext } | Should Not Throw
             }
         }
     }
     Describe 'Set-MsolTenantContext' {
         It 'Sets the correct Guid' {
-            Set-MsolTenantContext -TenantId $test_guid
-            $global:PSDefaultParameterValues['*-Msol*:TenantId'] | Should Be $test_guid
+            Set-MsolTenantContext -TenantId $global:test_guid
+            $global:PSDefaultParameterValues['*-Msol*:TenantId'] | Should Be $global:test_guid
         }
         It 'Throws an error with bad parameter' {
             { Set-MsolTenantContext -TenantId 'BAD IDEA' } | Should Throw
