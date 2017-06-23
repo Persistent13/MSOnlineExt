baa3697ddaa2e8c6492e45b4728f4295045a5a68
Dakota Clark <dakota@allclarks.com>
2017-06-22 19:16:22 -0700

    Fixed some dumb misses.

--- src/MSOnline-Extensions/MSOnline-Extensions.psm1
+++ src/MSOnline-Extensions/MSOnline-Extensions.psm1
@@ -1,4 +1,6 @@
 $completers_path = Join-Path -Path $PSScriptRoot -ChildPath 'Completers'
+$public_path = Join-Path -Path $PSScriptRoot -ChildPath 'Public'
+$public = @( Get-ChildItem -Path $public_path -Filter '*.ps1' )
 $completers = Get-ChildItem -Path $completers_path -Filter '*.Completer.ps1'
 
 foreach($item in $completers)
@@ -7,6 +9,20 @@ foreach($item in $completers)
     Write-Verbose -Message $message
     & $item.FullName
 }
+
+foreach($cmdlet in $public)
+{
+    try
+    {
+        . $cmdlet.FullName
+    }
+    catch
+    {
+        Write-Error -Message "Failed to import cmdlet $($cmdlet.FullName): $PSItem"
+    }
+}
+
+Export-ModuleMember -Function $public.BaseName
 
 # This code will run when the module is removed and
 # will be sure that the TenantId default is removed
